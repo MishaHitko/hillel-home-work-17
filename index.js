4884708574f69e67abfd6716e695b71192db6651
MishaHitko <mishagitko1997@gmail.com>
2022-12-13 18:14:29 +0300

    home work

--- index.js
+++ index.js
@@ -1,24 +1,75 @@
-const numArray = [1,2,3];
-const numbersArray = [1,2,[2.1,2.2,2.3],3];
-function generateList(array) {
-    const list = document.createElement('ul');
-    array.forEach( (value) =>{
-        if (!Array.isArray(value)) {
-            const item = document.createElement('li');
-            item.innerText = `${value}`;
-            list.append(item);
-        } else {
-            const item = document.createElement('li');
-            const miniList = document.createElement('ul');
-            list.append(item);
-            item.append(miniList);
-            value.forEach((value) => {
-                const miniListItem = document.createElement('li');
-                miniListItem.innerText = `${value}`;
-                miniList.append(miniListItem);
-            });
-        };
-    });
-    document.body.append(list);
+const btn = document.querySelector('.btnSave');
+const nameLastname = document.querySelector('.nameLastname');
+const date = document.querySelector('.date');
+const sex = document.querySelectorAll('.sex');
+const city = document.querySelector('select');
+const addr = document.querySelector('textarea');
+const language = document.querySelectorAll('.language');
+
+function name(obj) {
+    const finishNameLastName = document.createElement('p');
+    finishNameLastName.innerText = `Имя и Фамилия: ${obj.value}`;
+    document.body.append(finishNameLastName);
 };
-generateList(numArray);
+
+function finishDate(obj) {
+    const finishDat = document.createElement('p');
+    finishDat.innerText = `Дата рождения: ${obj.value}`;
+    document.body.append(finishDat);
+};
+
+function finishSex(array) {
+    const arrArray = [...array];
+    let sexValue = '';
+    arrArray.forEach((value) => {
+        if (value.checked) {
+            sexValue = value.value;
+        }
+        return sexValue;
+    });
+    const finalSex = document.createElement('p');
+    if (sexValue === 'men') {
+        finalSex.innerText = 'Ваш пол: мужской';
+    } else if (sexValue === 'women'){
+        finalSex.innerText = 'Ваш пол: женский';
+    } else {
+        finalSex.innerText = 'Вы не выбрали пол';
+    }
+    document.body.append(finalSex);
+};
+
+function finishCity(obj) {
+    const finalCity = document.createElement('p');
+    finalCity.innerText = `Ваш город: ${obj.value}`;
+    document.body.append(finalCity);
+};
+
+function finishAddr(obj) {
+    const finalAddr = document.createElement('p');
+    finalAddr.innerText = `Ваш адрес: ${obj.value}`;
+    document.body.append(finalAddr);
+};
+
+function finishLanguage(array) {
+    const finalLanguage = document.createElement('p');
+    const arrArray = [...array];
+    let languageValue = '';
+    arrArray.forEach((value) => {
+        if (value.checked) {
+            languageValue += `${value.value}  `;
+        }
+        return languageValue;
+    });
+    finalLanguage.innerText = `Вы владеете такими языкаки как: ${languageValue}`;
+    document.body.append(finalLanguage);
+};
+
+btn.addEventListener('click', () => {
+    document.body.innerHTML = '';
+    name(nameLastname);
+    finishDate(date);
+    finishSex(sex);
+    finishCity(city);
+    finishAddr(addr);
+    finishLanguage(language);
+});

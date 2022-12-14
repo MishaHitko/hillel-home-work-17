e67900584284e1634a5dd22096814666ecbc5030
MishaHitko <mishagitko1997@gmail.com>
2022-12-14 16:40:38 +0300

    home work

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-const link = document.querySelectorAll('.link')
-console.log(link);
-const linkArray = [...link];
-linkArray.forEach((value) => {
-    const links = value.getAttribute('href');
-   if (!links.includes('http://') && !links.includes('https://')) {
-       value.setAttribute('href', `https://${links}`);
-   }
-});
+class Symbol {
+    constructor(smile, wrapper) {
+        this.smile = smile;
+        this.wrapper = wrapper;
+        this.countVotes = 0;
+
+        this.createElement();
+        this.vote();
+    };
+
+    createElement() {
+        const block = document.createElement('div');
+        this.blockSmile = document.createElement('span');
+        this.count = document.createElement('p');
+
+        this.blockSmile.innerHTML = this.smile;
+        this.count.innerText = this.countVotes;
+
+        block.classList.add('blockSmile');
+        this.blockSmile.classList.add('smiles');
+
+        this.wrapper.append(block);
+        block.append(this.blockSmile);
+        block.append(this.count);
+    };
+
+    vote() {
+        this.blockSmile.addEventListener('click', () => {
+            this.countVotes += 1;
+            this.count.innerText = this.countVotes;
+        });
+    };
+};
+const wrapper = document.querySelector('.wrapper');
+
+const elem1 = new Symbol('&#9875;', wrapper);
+const elem2 = new Symbol('&#9990;', wrapper);
+const elem3 = new Symbol('&#9884;', wrapper);
+const elem4 = new Symbol('&#10026;', wrapper);
+const elem5 = new Symbol('&#9787;', wrapper);
